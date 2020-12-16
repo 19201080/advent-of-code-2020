60e56db77ea4034d21920d7f2b46339e46244605
19201080 <etienne.j.n@gmail.com>
2020-12-16 11:26:00 +0100

    Improve solution for day 15

--- day15/main.py
+++ day15/main.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from collections import defaultdict
+from time import time
+
+
 def get_content(name):
     with open(name, 'r') as file:
         return [int(el) for el in file.read().strip().split(',')]
 
 
-def process_data(content, rounds=2020):
-    cache = {val: (idx+1,) for idx, val in enumerate(content)}
-    data = content[:]
-    for i in range(rounds - len(data)):
-        last_rounds = cache.get(data[-1], ())
-        data.append(last_number := 0 if len(last_rounds) < 2 else
-                    last_rounds[-1] - last_rounds[-2])
-        cache[last_number] = cache.get(last_number, ())[-1:] + (len(data),)
-    return data[-1]
+def process_data(content, turns=2020):
+    cache = defaultdict(tuple, {v: (i+1,) for i, v in enumerate(content)})
+    last = content[-1]
+    for i in range(len(content), turns):
+        rounds = cache[last]
+        last = 0 if len(rounds) < 2 else rounds[1] - rounds[0]
+        cache[last] = cache[last][-1:] + (i+1,)
+    return last
 
 
 if __name__ == '__main__':
     file_content = get_content('input')
+    start = time()
     result = process_data(file_content)
-    print(f'problem 1 - the solution is: {result}')
+    print(f'problem 1 - the solution is: {result} (time: {time() - start:5f})')
 
+    start = time()
     result = process_data(file_content, 30000000)
-    print(f'problem 2 - the solution is: {result}')
+    print(f'problem 2 - the solution is: {result} (time: {time() - start:5f})')
