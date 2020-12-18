ff5d3062fdf9cc4a6f4a9c1621365d7ba6b5e047
19201080 <etienne.j.n@gmail.com>
2020-12-18 11:11:52 +0100

    Update solution for day 17

--- day17/main.py
+++ day17/main.py
@@ -2,10 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from collections import defaultdict
-from copy import deepcopy
+from itertools import product
 
 ACTIVE = '#'
-INACTIVE = '.'
 
 
 def get_content(name):
@@ -21,21 +20,15 @@ def do_round(content, dimensions):
         if v == ACTIVE:
             active_cubes.append(k)
     step = [-1, 0, 1]
-    if dimensions == 3:
-        directions = [(x, y, z)
-                      for x in step for y in step for z in step
-                      if x or y or z]
-    else:
-        directions = [(x, y, z, w)
-                      for x in step for y in step for z in step for w in step
-                      if x or y or z or w]
+    directions = [p for p in product(step, repeat=dimensions)
+                  if any(el for el in p)]
     neighbors = [[cube[idx] + direction[idx] for idx, el in enumerate(cube)]
                  for cube in active_cubes for direction in directions]
     counted = defaultdict(int)
     for location in neighbors:
         counted[tuple(location)] += 1
     neighbors = {k: v for k, v in counted.items() if 1 < v < 4}
-    new_actives = [k for k, v in neighbors.items() if k in active_cubes]
+    new_actives = [k for k in neighbors.keys() if k in active_cubes]
     new_actives += [k for k, v in neighbors.items()
                     if v == 3 and k not in active_cubes]
     return new_actives
@@ -46,9 +39,10 @@ def update_cube(actives):
     return result
 
 
-def run_rounds(content, rounds, dimensions=3):
-    new_content = deepcopy(content)
+def run_rounds(content, rounds=6):
+    new_content = content
     total_actives = 0
+    dimensions = len(list(content.keys())[0])
     for i in range(rounds):
         new_actives = do_round(new_content, dimensions)
         new_content = update_cube(new_actives)
@@ -58,11 +52,11 @@ def run_rounds(content, rounds, dimensions=3):
 
 def main():
     file_content = get_content('input')
-    result = run_rounds(file_content, 6)
+    result = run_rounds(file_content)
     print(f'problem 1 - the solution is: {result}')
 
     four_dimensions = {k + (0,): v for k, v in file_content.items()}
-    result = run_rounds(four_dimensions, 6, 4)
+    result = run_rounds(four_dimensions)
     print(f'problem 2 - the solution is: {result}')
 
 
